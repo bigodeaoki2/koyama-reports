8157c2e28e329dc6593d59aff8bd668d7455aefc
Gustavo Yukio <gyukiocc@gmail.com>
2020-10-14 04:11:47 -0300

    A

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/koyama-report'));
+app.use(express.static('./dist/koyama-relatorios'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname,'/dist/koyama-report/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/koyama-relatorios/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
